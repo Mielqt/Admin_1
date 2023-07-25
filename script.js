79051b836d7461be7862096f70bf0ad980c6d315
Amiel Medalla <amielmedalla26@gmail.com>
2023-07-25 18:25:11 +0800

    updating

--- script.js
+++ script.js
@@ -6,13 +6,4 @@ toggler.addEventListener("click",function(){
 var darkmode = document.querySelector("label");
 darkmode.addEventListener("click",changeBackground);
 
-function changeBackground(){
-    document.querySelector("body").style.backgroundColor = "black";
-}
 
-var lightmode = document.querySelector('label');
-lightmode.addEventListener("click", changeLightBackground);
-
-function changeLightBackground(){
-    document.querySelector(".content").style.backgroundColor = "white";
-}
